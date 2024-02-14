367211d33576e40ed699eef5e5d08b55b0243e7d
Yaniv Tawily <yaniv1tawily@gmail.com>
2024-02-14 14:28:55 +0200

    memory game update

--- memoryGame.js
+++ memoryGame.js
@@ -16,6 +16,8 @@ function animatePress(item) {
 }
 
 function start(boxs){
+  initialize(boxs);
+  
   numberOfCubes = boxs;
     document.getElementById("intraction").innerHTML = "<h1>Ok! lets start!!!</h1>";
     order = [];
@@ -23,6 +25,54 @@ function start(boxs){
     nextLevel();
 }
 
+function initialize(cubes){
+  let mainContainer = document.getElementById("mainContainer");
+  
+  switch (cubes) {
+    case 4:
+      mainContainer.innerHTML = '\
+      <div id="intraction"></div>\
+      <div class="rectangle4 btn" id="cube1"></div>\
+      <div class="rectangle4 btn" id="cube2"></div>\
+      <div class="rectangle4 btn" id="cube3"></div>\
+      <div class="rectangle4 btn" id="cube4"></div>';
+      mainContainer.className = "grid-container4";
+      ButtonsEventListener();
+      break;
+    case 6:
+      mainContainer.innerHTML = '\
+      <div id="intraction"></div>\
+      <div class="rectangle6 btn" id="cube1"></div>\
+      <div class="rectangle6 btn" id="cube2"></div>\
+      <div class="rectangle6 btn" id="cube3"></div>\
+      <div class="rectangle6 btn" id="cube4"></div>\
+      <div class="rectangle6 btn" id="cube5"></div>\
+      <div class="rectangle6 btn" id="cube6"></div>';
+      mainContainer.className = "grid-container6"
+      ButtonsEventListener();
+      break;
+    case 9: 
+      mainContainer.innerHTML = '\
+      <div id="intraction"></div>\
+      <div class="rectangle9 btn" id="cube1"></div>\
+      <div class="rectangle9 btn" id="cube2"></div>\
+      <div class="rectangle9 btn" id="cube3"></div>\
+      <div class="rectangle9 btn" id="cube4"></div>\
+      <div class="rectangle9 btn" id="cube5"></div>\
+      <div class="rectangle9 btn" id="cube6"></div>\
+      <div class="rectangle9 btn" id="cube7"></div>\
+      <div class="rectangle9 btn" id="cube8"></div>\
+      <div class="rectangle9 btn" id="cube9"></div>';
+      mainContainer.className = "grid-container9"
+      ButtonsEventListener();
+      break;
+  }
+
+
+
+
+
+}
 function nextLevel(){
   nextPressed = -1;
   nextPress();
@@ -44,10 +94,14 @@ function nextPress(){
 }
 
 
-/// Button event Event Listener ///
+function ButtonsEventListener(){
+  const buttons = Array.from(document.querySelectorAll('.btn'));
+  
+  buttons.forEach(button => {
+    button.addEventListener('click', handleClick);
+  });
+}
 
-
-const buttons = Array.from(document.querySelectorAll('div.rectangle'));
 
 // Function to handle the click event
 function handleClick(event) {
@@ -60,24 +114,17 @@ function handleClick(event) {
   }
 }
 
-// Attach click event listener to each button
-buttons.forEach(button => {
-  button.addEventListener('click', handleClick);
-});
+
 
 function checkAnswer(pressed){
-  // console.log(order[nextPressed]);
-  // console.log(pressed);
-  // console.log(order);
-  // console.log(nextPressed);
 
   if("cube" + order[nextPressed] == pressed){
     if(order.length == (nextPressed+1))
     {
-      document.getElementById("intraction").style.backgroundColor = "green";
+      document.getElementById("intraction").style.backgroundColor = "Yes! Level up!";
       setTimeout(nextLevel, 1000);
     }else{
-      document.getElementById("intraction").style.backgroundColor = "orange";
+      document.getElementById("intraction").style.backgroundColor = "Keep Going";
       nextPressed++;
     }
   }else{
@@ -86,7 +133,16 @@ function checkAnswer(pressed){
 }
 
 function fail(){
-  document.getElementById("intraction").style.backgroundColor = "red";
-  document.getElementById("intraction").innerHTML = "<h1> Game over! your score is " + (order.length* 100) + "</h1>";
+  const element = document.getElementById("intraction")
+  element.style.backgroundColor = "red";
+  element.innerHTML = "<h1> Game over! your score is " + (order.length* 100) + "</h1>";
+  element.innerHTML += "<button onclick='restart()'>Try again</button>"
   userTurn = false;
+}
+
+function restart(){
+  document.getElementById("intraction").innerHTML = "<h1>ready to start? please choose the difficulty of the game</h1>\
+  <button onclick='start(4)'>Easy - 4 buttons</button>\
+  <button onclick='start(6)'>Normal - 6 buttons</button>\
+  <button onclick='start(9)'>Hard - 9 buttons</button>";
 }
