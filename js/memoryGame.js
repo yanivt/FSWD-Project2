489aaf24d382cf032e3e8c11e0570794e407413c
Yaniv Tawily <yaniv1tawily@gmail.com>
2024-02-14 15:45:29 +0200

    memory game 95%

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -4,78 +4,90 @@ let nextPressed = 0;
 let user;
 let userTurn = false;
 let numberOfCubes = 6;
+let intraction = document.getElementById("intraction");
+let audioPlayer = document.getElementById("audioPlayer");
+
+function updateIntraction(){
+  intraction = document.getElementById("intraction");
+}
 
 function animateRec(item){
     document.getElementById(item).style.opacity =1;
     setTimeout(() => document.getElementById(item).style.opacity =0.77, 1000);
-    // add sound
+    
+    audioPlayer.src = "../media/sounds/gamePress.wav";
+    audioPlayer.play();
 }
 function animatePress(item) {
     document.getElementById(item).style.opacity =1;
     setTimeout(() => document.getElementById(item).style.opacity =0.77, 500);
+
+    audioPlayer.src = "../media/sounds/userPress.wav";
+    audioPlayer.play();
 }
 
-function start(boxs){
-  initialize(boxs);
+function startGame(cubes){
+  initialize(cubes);
+  intraction.innerHTML = "<h1>Ok! lets start!!!</h1>";
   
-  numberOfCubes = boxs;
-    document.getElementById("intraction").innerHTML = "<h1>Ok! lets start!!!</h1>";
-    order = [];
-
-    nextLevel();
+  nextLevel();
 }
 
 function initialize(cubes){
   let mainContainer = document.getElementById("mainContainer");
-  
-  switch (cubes) {
+  order = [];
+  numberOfCubes = cubes;
+
+  switch (numberOfCubes) {
     case 4:
       mainContainer.innerHTML = '\
       <div id="intraction"></div>\
-      <div class="rectangle4 btn" id="cube1"></div>\
-      <div class="rectangle4 btn" id="cube2"></div>\
-      <div class="rectangle4 btn" id="cube3"></div>\
-      <div class="rectangle4 btn" id="cube4"></div>';
-      mainContainer.className = "grid-container4";
+      <div class="rectangle" id="cube1"></div>\
+      <div class="rectangle" id="cube2"></div>\
+      <div class="rectangle" id="cube3"></div>\
+      <div class="rectangle" id="cube4"></div>';
+      mainContainer.className = "grid-container";
       ButtonsEventListener();
+      updateIntraction();
       break;
     case 6:
       mainContainer.innerHTML = '\
       <div id="intraction"></div>\
-      <div class="rectangle6 btn" id="cube1"></div>\
-      <div class="rectangle6 btn" id="cube2"></div>\
-      <div class="rectangle6 btn" id="cube3"></div>\
-      <div class="rectangle6 btn" id="cube4"></div>\
-      <div class="rectangle6 btn" id="cube5"></div>\
-      <div class="rectangle6 btn" id="cube6"></div>';
-      mainContainer.className = "grid-container6"
+      <div class="rectangle" id="cube1"></div>\
+      <div class="rectangle" id="cube2"></div>\
+      <div class="rectangle" id="cube3"></div>\
+      <div class="rectangle" id="cube4"></div>\
+      <div class="rectangle" id="cube5"></div>\
+      <div class="rectangle" id="cube6"></div>';
+      mainContainer.className = "grid-container"
       ButtonsEventListener();
+      updateIntraction();
       break;
     case 9: 
       mainContainer.innerHTML = '\
       <div id="intraction"></div>\
-      <div class="rectangle9 btn" id="cube1"></div>\
-      <div class="rectangle9 btn" id="cube2"></div>\
-      <div class="rectangle9 btn" id="cube3"></div>\
-      <div class="rectangle9 btn" id="cube4"></div>\
-      <div class="rectangle9 btn" id="cube5"></div>\
-      <div class="rectangle9 btn" id="cube6"></div>\
-      <div class="rectangle9 btn" id="cube7"></div>\
-      <div class="rectangle9 btn" id="cube8"></div>\
-      <div class="rectangle9 btn" id="cube9"></div>';
+      <div class="rectangle" id="cube1"></div>\
+      <div class="rectangle" id="cube2"></div>\
+      <div class="rectangle" id="cube3"></div>\
+      <div class="rectangle" id="cube4"></div>\
+      <div class="rectangle" id="cube5"></div>\
+      <div class="rectangle" id="cube6"></div>\
+      <div class="rectangle" id="cube7"></div>\
+      <div class="rectangle" id="cube8"></div>\
+      <div class="rectangle" id="cube9"></div>';
       mainContainer.className = "grid-container9"
       ButtonsEventListener();
+      updateIntraction();
       break;
   }
 
-
-
-
-
 }
 function nextLevel(){
+  audioPlayer.src = "../media/sounds/levelUp.wav";
+  audioPlayer.play();
+
   nextPressed = -1;
-  nextPress();
+  setTimeout(nextPress, 1000);
 }
 
 function nextPress(){
@@ -83,19 +95,24 @@ function nextPress(){
   userTurn = false;
   let newPress = Math.floor(Math.random() * numberOfCubes) + 1;
   order.push(newPress);
-  document.getElementById("intraction").innerHTML += order;
-  
+  intraction.innerHTML = "<h1>wait for it..</h1>";
+  intraction.style.color = "blue";
+  let time;
   order.forEach((element, index) => {
+    time = index * 1000;
+
     setTimeout(() => {
         animateRec("cube" + element);
-    }, index * 1000);
+    }, time);
 });
-  setTimeout(() => userTurn = true, 1000);
+  setTimeout(() => userTurn = true, time);
+  setTimeout(() => intraction.innerHTML = "<h1>your turn..</h1>", time);
+  intraction.style.backgroundColor = "orange";
 }
 
 
 function ButtonsEventListener(){
-  const buttons = Array.from(document.querySelectorAll('.btn'));
+  const buttons = Array.from(document.querySelectorAll('.rectangle'));
   
   buttons.forEach(button => {
     button.addEventListener('click', handleClick);
@@ -121,10 +138,13 @@ function checkAnswer(pressed){
   if("cube" + order[nextPressed] == pressed){
     if(order.length == (nextPressed+1))
     {
-      document.getElementById("intraction").style.backgroundColor = "Yes! Level up!";
+      intraction.innerHTML = "<h1>Yes! Level up!</h1>";
+      intraction.style.backgroundColor = "green";
       setTimeout(nextLevel, 1000);
     }else{
-      document.getElementById("intraction").style.backgroundColor = "Keep Going";
+      intraction.innerHTML = "<h1>Keep Going</h1>";
+      intraction.style.backgroundColor = "orange";
+
       nextPressed++;
     }
   }else{
@@ -133,16 +153,17 @@ function checkAnswer(pressed){
 }
 
 function fail(){
-  const element = document.getElementById("intraction")
-  element.style.backgroundColor = "red";
-  element.innerHTML = "<h1> Game over! your score is " + (order.length* 100) + "</h1>";
-  element.innerHTML += "<button onclick='restart()'>Try again</button>"
+  intraction.style.backgroundColor = "red";
+  intraction.innerHTML = "<h1> Game over! your score is " + (order.length* 100) + "</h1>";
+  intraction.innerHTML += "<button onclick='restart()'>Try again</button>"
   userTurn = false;
+  audioPlayer.src = "../media/sounds/gameOver.mp3";
+  audioPlayer.play();
 }
 
 function restart(){
-  document.getElementById("intraction").innerHTML = "<h1>ready to start? please choose the difficulty of the game</h1>\
-  <button onclick='start(4)'>Easy - 4 buttons</button>\
-  <button onclick='start(6)'>Normal - 6 buttons</button>\
-  <button onclick='start(9)'>Hard - 9 buttons</button>";
+  intraction.innerHTML = "<h1>ready to start? please choose the difficulty of the game</h1>\
+  <button onclick='startGame(4)'>Easy - 4 buttons</button>\
+  <button onclick='startGame(6)'>Normal - 6 buttons</button>\
+  <button onclick='startGame(9)'>Hard - 9 buttons</button>";
 }
